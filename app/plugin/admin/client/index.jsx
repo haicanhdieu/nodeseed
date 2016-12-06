4ebed60cf29760487e0f114821f0436f48ba7e2e
MinhNguyen <ntm.xyz@gmail.com>
2016-12-06 23:06:51 +0700

    [8162993]: seting up react-route

--- app/plugin/admin/client/index.jsx
+++ app/plugin/admin/client/index.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReactRouter from 'react-router'
 import Base from './components/layout/base.jsx';
+import Dashboard from './components/layout/base-dashboard.jsx';
+import Login from './components/login/login.jsx';
+
+const Router = ReactRouter.Router;
 
 ReactDOM.render(
-    <Base />,
+    <Router history={browserHistory}>
+
+    </Router>,
     document.getElementById('container')
 );
