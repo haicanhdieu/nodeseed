a4aa16b4da216d490a3c0755958c13f1124a4c3a
MinhNguyen <ntm.xyz@gmail.com>
2016-12-11 09:53:34 +0700

    add react-router, change to use npm instead of bower

--- app/plugin/admin/client/components/layout/base.jsx
+++ app/plugin/admin/client/components/layout/base.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import { Link } from 'react-router'
 
 export default class Base extends React.Component {
     render() {
         return (
             <div>
-                <Link to="/dashboard">Dashboard</Link>
                 {this.props.children}
             </div>
         )
--- app/plugin/admin/client/index.jsx
+++ app/plugin/admin/client/index.jsx
@@ -1,11 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ReactRouter from 'react-router'
-import Base from './components/layout/base.jsx';
-import Dashboard from './components/layout/base-dashboard.jsx';
-import Login from './components/login/login.jsx';
-
-const Router = ReactRouter.Router;
+import { Router, browserHistory } from 'react-router'
 
 ReactDOM.render(
     <Router history={browserHistory}>
